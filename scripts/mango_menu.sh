eb12d42c7b032c67cf70b5cb1d9826b606151832
Paul Gollhofer <paul.gollhofer09@htl-salzburg.ac.at>
2026-09-14 13:41:39 +0200

    Delete scripts directory

--- scripts/mango_menu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Get list of presets from the folder names
-CHOICE=$(ls ~/.config/mango_presets | wofi --show dmenu --prompt "Choose Desktop Style")
-
-if [ -n "$CHOICE" ]; then
-    ~/switch_mango.sh "$CHOICE"
-fi
